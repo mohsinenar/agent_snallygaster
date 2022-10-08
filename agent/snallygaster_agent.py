0cde522752f1a8d8acab68ba07c17cdb0b625d96
mohsineos01 <mnarmouche@gmail.com>
2022-10-08 14:58:12 +0100

    init.

---
+++ agent/snallygaster_agent.py
@@ -0,0 +1,62 @@
+"""dnsReaper [https://github.com/punk-security/dnsReaper](dnsReaper) tool implementation as ostorlab agent"""
+import json
+import logging
+import subprocess
+from typing import List, Dict, Any
+
+from ostorlab.agent import agent, definitions as agent_definitions
+from ostorlab.agent.kb import kb
+from ostorlab.agent.message import message as msg
+from ostorlab.agent.mixins import agent_persist_mixin as persist_mixin
+from ostorlab.agent.mixins import agent_report_vulnerability_mixin as vuln_mixin
+from ostorlab.runtimes import definitions as runtime_definitions
+from rich import logging as rich_logging
+
+logging.basicConfig(
+    format='%(message)s',
+    datefmt='[%X]',
+    level='INFO',
+    force=True,
+    handlers=[rich_logging.RichHandler(rich_tracebacks=True)]
+)
+logger = logging.getLogger(__name__)
+logger.setLevel('DEBUG')
+
+
+def _run_snallygaster_command(domain: str) -> Any:
+    command = ['snallygaster', domain, '-j', '-n']
+    logger.info('running dnsReaper with command "%s"', ' '.join(command))
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+    logger.info('dnsReaper finished')
+    return json.loads(proc.stdout.read())
+
+
+class SnallyGasterAgent(agent.Agent, vuln_mixin.AgentReportVulnMixin, persist_mixin.AgentPersistMixin):
+    """Process the message and emit the findings"""
+
+    def __init__(self, agent_definition: agent_definitions.AgentDefinition,
+                 agent_settings: runtime_definitions.AgentSettings) -> None:
+        agent.Agent.__init__(self, agent_definition, agent_settings)
+        vuln_mixin.AgentReportVulnMixin.__init__(self)
+        persist_mixin.AgentPersistMixin.__init__(self, agent_settings)
+
+    def process(self, message: msg.Message) -> None:
+        """Process only message of type v3.asset.domain_name"""
+        domain_name: str = message.data.get('name', '')
+        if domain_name is not None and self.set_add(b'agent_snallygaster', f'{domain_name}'):
+            logger.info('processing domain name: %s', domain_name)
+            findings = _run_snallygaster_command(domain_name)
+            self._emit_findings(findings)
+
+    def _emit_findings(self, findings: List[Dict[Any, Any]]) -> None:
+        """Emit findings as a vulnerability"""
+        for finding in findings:
+            technical_detail = f"""```{finding}```"""
+            self.report_vulnerability(entry=kb.KB.WEB_GENERIC,
+                                      technical_detail=technical_detail,
+                                      risk_rating=vuln_mixin.RiskRating.LOW)
+
+
+if __name__ == '__main__':
+    logger.info('starting agent dnsReaper ...')
+    SnallyGasterAgent.main()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,30 @@
-"""
-    Dummy conftest.py for template_agent.
+"""conftest.py for agent Fingerprint Generator. """
+import pathlib
 
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    - https://docs.pytest.org/en/stable/fixture.html
-    - https://docs.pytest.org/en/stable/writing_plugins.html
-"""
+import pytest
+from ostorlab.agent import definitions as agent_definitions
+from ostorlab.agent.message import message as msg
+from ostorlab.runtimes import definitions as runtime_definitions
 
-# import pytest
+from agent import snallygaster_agent
+
+OSTORLAB_YAML_PATH = (pathlib.Path(__file__).parent.parent / 'ostorlab.yaml').absolute()
+
+
+@pytest.fixture
+def scan_message_domain_name() -> msg.Message:
+    """Creates a dummy message of type v3.asset.ip.v4.port.service to be used by the agent for testing purposes.
+    """
+    selector = 'v3.asset.domain_name'
+    msg_data = {'name': ' zero.webappsecurity.com'}
+    return msg.Message.from_data(selector, data=msg_data)
+
+
+@pytest.fixture
+def test_agent() -> snallygaster_agent.SnallyGasterAgent:
+    with open(OSTORLAB_YAML_PATH, 'r', encoding='utf-8') as yaml_o:
+        definition = agent_definitions.AgentDefinition.from_yaml(yaml_o)
+        settings = runtime_definitions.AgentSettings(
+            key='agent/ostorlab/agent_snallygaster_generator',
+            redis_url='redis://redis')
+        return snallygaster_agent.SnallyGasterAgent(definition, settings)
